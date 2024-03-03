72d961e8d4f89464ff2397cb27deb231e6899b1b
miladaleali <miladaleali1392@gmail.com>
2024-03-03 10:47:08 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name="endurance",
+        version="0.0.0",
+        author="Milad Aleali",
+        author_email="miladaleali1392@gmail.com",
+        packages=["endurance"],
+        package_dir={"endurance":"endurance"},
+        url="http://github.com/miladaleali/endurance/",
+        license="MIT",
+        install_requires=[]
+)
